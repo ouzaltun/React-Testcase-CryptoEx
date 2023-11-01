f53bfa600083bbf5223464add9afe876ed964d7b
Oğuzhan Altun <altunnoguzhan@gmail.com>
2023-11-02 00:08:42 +0300

    updated
    updated

--- React-Crypto-TestCase/vite.config.js
+++ React-Crypto-TestCase/vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "build", // Üretim dosyalarının çıkacağı klasörü belirtin
+    outDir: "dist", // Üretim dosyalarının çıkacağı klasörü belirtin
   },
 });
